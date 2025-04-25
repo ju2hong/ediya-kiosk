51b472e0847c41788167c5d23c98b7d74ea79fbf
ju2hong <juihong730@gmail.com>
2025-04-25 17:44:28 +0900

    be

---
+++ mcdonald_kiosk_be/src/main/java/com/kiosk/mcdonald_kiosk_be/McdonaldKioskBeApplication.java
@@ -0,0 +1,13 @@
+package com.kiosk.mcdonald_kiosk_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McdonaldKioskBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McdonaldKioskBeApplication.class, args);
+	}
+
+}
